7418200def85eddd3b4c77334c4334d6619be851
harshit0571 <harshitchadha65@gmail.com>
2023-08-27 15:01:52 +0530

    header created responsive

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".ham-btn").click(function () {
+    $(".header-links").toggleClass("active");
+
+    if ($(".header-links").hasClass("active")) {
+      $(".ham-btn").html(`<i class="fa fa-times"></i>`);
+    } else {
+      $(".ham-btn").html(`<i class="fa fa-solid fa-bars"></i>`);
+    }
+  });
+});
